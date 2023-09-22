3eea7f30ad9fa2930a58111666177b52a59f1dad
andrii-sheremet <andrii.sheremet@nure.ua>
2023-09-22 22:08:32 +0200

    .

---
+++ Data Base (kurs)/Class/Cats.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Base__kurs_.Class
+{
+    internal struct Cat
+    {
+        // Основні властивості кота
+        public int Id { get; set; }
+        public string Name { get; set; } // Ім'я кота
+        public string Breed { get; set; } // Порода
+        public int Age { get; set; } // Вік
+        public string Color { get; set; } // Колір шерсті
+        public string Description { get; set; } // Опис кота
+
+        // Додаткові властивості для характеристик породи
+        public string Origin { get; set; } // Походження породи
+        public string Temperament { get; set; } // Темперамент
+        public string Lifespan { get; set; } // Тривалість життя
+        public string Width { get; set; } // Вага
+        public string Size { get; set; } // Розмір
+        public string Grooming { get; set; } // Догляд за шерстю
+    }
+}
---
+++ Data Base (kurs)/Forms/EditPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Data_Base__kurs_.Forms
+{
+    public partial class EditPage : Form
+    {
+        public EditPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
